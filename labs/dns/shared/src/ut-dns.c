e517e4a191899c6be474121a83fb3fbda81aa559
zhxie <zhxie@node0.zhxie-esapd08.utcs356-pg0.utah.cloudlab.us>
2024-04-14 21:53:34 -0600

    ut dns draft

--- labs/dns/shared/src/ut-dns.c
+++ labs/dns/shared/src/ut-dns.c
@@ -23,24 +23,59 @@ int main() {
     /* PART1 TODO: Implement a DNS nameserver for the utexas.edu zone */
     
     /* 1. Create an **UDP** socket */
-    
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
     /* 2. Initialize server address (INADDR_ANY, DNS_PORT) */
     /* Then bind the socket to it */
 
+    memset(&server_addr, 0, sizeof(server_addr));
+    memset(&client_addr, 0, sizeof(client_addr));
+
+    // Filling server information
+    server_addr.sin_family = AF_INET; // IPv4
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(DNS_PORT);
+
+    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
     /* 3. Initialize a server context using TDNSInit() */
     /* This context will be used for future TDNS library function calls */
+
+    struct TDNSServerContext *ctx = TDNSInit();
 
     /* 4. Create the utexas.edu zone using TDNSCreateZone() */
     /* Add an IP address for www.utexas.edu domain using TDNSAddRecord() */
     /* Add the UTCS nameserver ns.cs.utexas.edu using using TDNSAddRecord() */
     /* Add an IP address for ns.cs.utexas.edu domain using TDNSAddRecord() */
 
+    TDNSCreateZone(ctx, "utexas.edu");
+    TDNSAddRecord(ctx, "utexas.edu", "www", "23.185.0.4", NULL);
+    TDNSAddRecord(ctx, "utexas.edu", "cs", NULL, "ns.cs.utexas.edu");
+    TDNSAddRecord(ctx, "cs.utexas.edu", "ns", "128.83.120.48", NULL);
+
     /* 5. Receive a message continuously and parse it using TDNSParseMsg() */
+
+    while (1) {
+        int len, n;
+
+        len = sizeof(client_addr); // len is value/result
+        n = recvfrom(sockfd, buffer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&client_addr, &len);
+        buffer[n] = '\0'; // Null-terminate the received message
+
+        // Parse the received message
+        printf("Message is %s\n", buffer);
+    }
 
     /* 6. If it is a query for A, AAAA, NS DNS record */
     /* find the corresponding record using TDNSFind() and send the response back */
     /* Otherwise, just ignore it. */
+
 
     return 0;
 }
