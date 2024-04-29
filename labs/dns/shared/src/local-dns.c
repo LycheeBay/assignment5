50a5678b9604cc8c04fcd9b9bd03b54dfdf61bcb
zhxie <zhxie@node0.zhxie-esapd08.utcs356-pg0.utah.cloudlab.us>
2024-04-28 21:48:14 -0600

    local-dns stub

--- labs/dns/shared/src/local-dns.c
+++ labs/dns/shared/src/local-dns.c
@@ -59,17 +59,73 @@ int main() {
     
     /* 1. Create an **UDP** socket */
 
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
     /* 2. Initialize server address (INADDR_ANY, DNS_PORT) */
     /* Then bind the socket to it */
 
+    memset(&server_addr, 0, sizeof(server_addr));
+    memset(&client_addr, 0, sizeof(client_addr));
+
+    // Filling server information
+    server_addr.sin_family = AF_INET; // IPv4
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(DNS_PORT);
+
+    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
     /* 3. Initialize a server context using TDNSInit() */
     /* This context will be used for future TDNS library function calls */
+
+    struct TDNSServerContext *ctx = TDNSInit();
 
     /* 4. Create the edu zone using TDNSCreateZone() */
     /* Add the UT nameserver ns.utexas.edu using using TDNSAddRecord() */
     /* Add an IP address for ns.utexas.edu domain using TDNSAddRecord() */
 
+    TDNSCreateZone(ctx, "edu");
+    TDNSAddRecord(ctx, "utexas.edu", "ns", NULL, "ns.utexas.edu");
+    TDNSAddRecord(ctx, "ns.utexas.edu", "ns", "40.0.0.20", NULL);
+
     /* 5. Receive a message continuously and parse it using TDNSParseMsg() */
+
+    while (1) {
+        int len, n;
+
+        len = sizeof(client_addr); // len is value/result
+        n = recvfrom(sockfd, buffer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&client_addr, &len);
+        buffer[n] = '\0'; // Null-terminate the received message
+
+        // Parse the received message
+        struct TDNSParseResult *parsed = malloc(sizeof(struct TDNSParseResult));;
+        if (parsed == NULL) {
+            perror("malloc failed");
+            exit(EXIT_FAILURE);
+        }
+        TDNSParseMsg(buffer, sizeof(buffer), parsed);
+
+        struct TDNSFindResult *res = malloc(sizeof(struct TDNSFindResult)); 
+        if (res == NULL) {
+            perror("malloc failed");
+            exit(EXIT_FAILURE);
+        }
+
+        if (parsed->qtype == 1 || parsed->qtype == 2 || parsed->qtype == 28) {  // A, NS, AAAA
+            TDNSFind(ctx, parsed, res);
+            //sendto(sockfd, res, sizeof(res), 0);
+            sendto(sockfd, res, sizeof(struct TDNSFindResult), 0, (struct sockaddr *)&client_addr, client_len);
+            //return 0;
+        }
+
+        free(parsed);
+        free(res);
+    }
 
     /* 6. If it is a query for A, AAAA, NS DNS record, find the queried record using TDNSFind() */
     /* You can ignore the other types of queries */
